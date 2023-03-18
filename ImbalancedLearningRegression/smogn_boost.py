f626e02c3b650519c5df6ca41d156d901b529431
Amit <62823947+AmitShanbhoug@users.noreply.github.com>
2023-03-18 15:49:11 -0400

    Updated with local changes

--- ImbalancedLearningRegression/smogn_boost.py
+++ ImbalancedLearningRegression/smogn_boost.py
@@ -34,32 +34,36 @@ def smogn_boost(T, data, perc, pert, replace, y, threshold):
     while iteration <= T:
 
         # this is the initiial iteration of smogn, calculating it for the bumps, giving new data oversampled
-        dt_over_sampled = gn(data=data, index=list(), pert=pert, replace=replace)
+        dt_over_sampled = smogn(data=data, index=list(), pert=pert, replace=replace)
 
-        # this is to call the decision tree, predict regression value for y, and return the predicted values
+        # this is to call the decision tree and use it to achieve a new model, predict regression value for y (target response variable), and return the predicted values
         dt_data = tree.DecisionTreeRegressor()
+        
         dt_data_predictions = dt_data.predict(dt_over_sampled, y)
-
-        # calculating error rate
 
         #initialize error rate
         error = 0
 
-            for each y in dt_data:
-                if
-
-        error = abs((dt_data[y] - data_new[y])/dt_data[y])
-
+        # calcualte the error rate of the new model achieved earlier, as the delta between original dataset and predicted oversampled dataset
+        error = abs((dt_data[y] - dt_data_predictions[y])/dt_data[y])
+             
+        # beta is the update parameter of weights based on the error rate calculated
         beta = pow(error, 2)
+     
+        # we compare the error rate to a given threshold (by the user) and update the weight parameter based on this info
+        for y in dt_data:
+            if error <= threshold:
+                dt_data[perc] = beta
+            else:
+                dt_data[perc] = 1
 
         # update distribution with new weights, weights are beta
-        dt_updated = pd.concat([perc, beta], ignore_index=True)
+        # dt_updated = pd.concat([perc, beta], ignore_index=True)
 
         # apply normalization factor using sci kit learn
-        dt_normalized = preprocessing.normalize(dt_updated, max)
+        dt_normalized = preprocessing.normalize(dt_data, max)
 
         # iteration count
         iteration = iteration + 1
-
-
-    ):
+    
+    return smogn_boost

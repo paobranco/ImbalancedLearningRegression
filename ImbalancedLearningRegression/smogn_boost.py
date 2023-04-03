19fd24b62b20ce8acc7e9fc596c102ff3e19957c
Amit <62823947+AmitShanbhoug@users.noreply.github.com>
2023-04-03 11:36:24 -0400

    Update

--- ImbalancedLearningRegression/smogn_boost.py
+++ ImbalancedLearningRegression/smogn_boost.py
@@ -6,6 +6,7 @@ import math
 import smogn as smogn
 from sklearn import tree
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 # load dependencies - internal
 from ImbalancedLearningRegression.phi import phi
@@ -14,28 +15,26 @@ from ImbalancedLearningRegression.over_sampling_gn import over_sampling_gn
 from ImbalancedLearningRegression.gn import gn
 
 # synthetic minority over-sampling technique for regression with gaussian noise with boost (based on SMOTEBoost using Adaboost)
+# Look at https://github.com/nunompmoniz/ReBoost/blob/master/R/Functions.R
 
 # ****need a train and a test set****
 def smogn_boost(TotalIterations, data, pert, replace, k, y, error_threshold, rel_thres, samp_method = "balance"):
 
-    # arguments / inputs
-    # TotalIterations: number of iterations, user inputted integer value
-    # data,  # training set
-    # pert,  # perturbation / noise percentage
-    # replace,  # sampling replacement (bool)
-    # y, # response variable y by name (string)
-    # error_threshold:  error threshold defined by the user
-    # samp_method: "balance or extreme" - samp method is perc
-    # rel_thres: relevance threshold defined by the user
+    # arguments/inputs
+    # TotalIterations: user defined total number of iterations (pos int)
+    # data: training set
+    # pert: perturbation / noise percentage
+    # replace: sampling replacement (bool)
+    # k: num of neighs for over-sampling (pos int)
+    # y: response variable y by name (string)
+    # error_threshold: user defined error threshold 
+    # rel_thres: user defined relevance threshold 
+    # samp_method: "balance or extreme" - sampling method is perc
 
     # set an initial iteration
     iteration = 1
     
-    # set distribution as 1/m weights, which is length of data -1, as one of them is the target variable y
-    
-    # Look at https://github.com/nunompmoniz/ReBoost/blob/master/R/Functions.R
-    # ******need to use number of rows here******
-    # convert data to data frame here
+    # set distribution as 1/m weights, which is length of data -1, as one of them is the target variable y 
     weights = 1/(len(data))
     dt_distribution = []
     for i in len(data):
@@ -43,8 +42,9 @@ def smogn_boost(TotalIterations, data, pert, replace, k, y, error_threshold, rel
 
     # calling phi control
     pc = phi_ctrl_pts (y=y, method="manual", xtrm_type = "both", coeff = 1.5, ctrl_pts=any)
-    # calling the control points only from the output
-    rel_ctrl_pts_rg = pc[3]
+    
+    # calling only the control points (third value) from the output
+    rel_ctrl_pts_rg = pc[2]
     
     # loop while iteration is less than user provided iterations
     while iteration <= TotalIterations:
@@ -52,18 +52,21 @@ def smogn_boost(TotalIterations, data, pert, replace, k, y, error_threshold, rel
         # this is the initial iteration of smogn, calculating it for the bumps, giving new data oversampled
         dt_over_sampled = smogn(data=data, y = y, k = 5, pert = pert, replace=replace, rel_thres = rel_thres, rel_method = "manual", rel_ctrl_pts_rg = rel_ctrl_pts_rg)
 
+        # split oversampled data into a training and test set
+        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
+
         # this is to call the decision tree and use it to achieve a new model, predict regression value for y (target response variable), and return the predicted values
         dt_model = tree.DecisionTreeRegressor()
         
-        #check if I need to separate features and target
+        # check if I need to separate features and target
         dt_model = dt_model.fit(dt_over_sampled) 
         dt_data_predictions = dt_model.predict(y)
 
-        #initialize error rate
+        # initialize error rate
         error = 0
 
         # calculate the error rate of the new model achieved earlier, as the delta between original dataset and predicted oversampled dataset
-        #for each y in the dataset, calculate whether it is greater/lower than threshold and update accordingly
+        # for each y in the dataset, calculate whether it is greater/lower than threshold and update accordingly
         error = abs((data[y] - dt_data_predictions[y])/data[y])
         
         for i in range(1, len[dt_data_predictions], 1):
@@ -73,19 +76,20 @@ def smogn_boost(TotalIterations, data, pert, replace, k, y, error_threshold, rel
         # beta is the update parameter of weights based on the error rate calculated
         beta = pow(epsilon_t, 2)
 
-        # update the distribution
+        # update the distribution weights
         for i in dt_distribution:
             if error[i] <= error_threshold:
                 dt_distribution[i] = dt_distribution[i] * beta
             else:
                 dt_distribution[i] = dt_distribution[i]
 
-        # apply normalization factor using sci kit learn
+        # normalize the distribution 
         dt_normalized = preprocessing.normalize(dt_distribution, max)
 
         # iteration count
         iteration = iteration + 1
 
     # **** need to modify original data after the whole loop ****
+    
     
     return smogn_boost

94c7547c5856bc06bffc8d78f946d0fc1b9b43a4
Amit <62823947+AmitShanbhoug@users.noreply.github.com>
2023-04-10 17:43:22 -0400

    Update

--- ImbalancedLearningRegression/smogn_boost.py
+++ ImbalancedLearningRegression/smogn_boost.py
@@ -17,32 +17,30 @@ from ImbalancedLearningRegression.gn import gn
 # synthetic minority over-sampling technique for regression with gaussian noise with boost (based on SMOTEBoost using Adaboost)
 # Look at https://github.com/nunompmoniz/ReBoost/blob/master/R/Functions.R
 
-def smogn_boost(data, test_data, TotalIterations, pert, replace, k, y, error_threshold, rel_thres, samp_method = "balance"):
+def smogn_boost(data, test_data, y, TotalIterations, pert, replace, k, error_threshold, rel_thres, samp_method = "balance"):
 
     # arguments/inputs
     
     # data: training set
     # test_data: test data
-    # Y_test: target variable from test data
+    # y: response variable y by name (string)
     # TotalIterations: user defined total number of iterations (pos int)
     # pert: perturbation / noise percentage
     # replace: sampling replacement (bool)
     # k: num of neighs for over-sampling (pos int)
-    # y: response variable y by name (string)
     # error_threshold: user defined error threshold 
     # rel_thres: user defined relevance threshold 
     # samp_method: "balance or extreme" - sampling method is perc
-
-    # pre-processing the test_data, reference: https://subscription.packtpub.com/book/data/9781838552862/1/ch01lvl1sec10/train-and-test-data
-    # read the test data and split features (X) and target value (Y)
-    df = pd.read_csv(test_data, header = 0)
-    X_test = df.drop('y', axis = 1)
-    Y_test = df['y']
     
-    # read the data and split features (X) and target value (Y)
-    df = pd.read_csv(data, header = 0)
-    X_data = df.drop('y', axis = 1)
-    Y_data = df['y']
+    # read the test data and split features (X) and target value (Y), reference: https://subscription.packtpub.com/book/data/9781838552862/1/ch01lvl1sec10/train-and-test-data
+    df_testData = pd.read_csv(test_data, header = 0)
+    X_test = df_testData.drop(y, axis = 1)
+    Y_test = df_testData[y]
+    
+    # read the training data and split features (X) and target value (Y)
+    df_data = pd.read_csv(data, header = 0)
+    X_data = df_data.drop(y, axis = 1)
+    Y_data = df_data[y]
 
     # set for clarity, name of target variable not data
     y_train = y
@@ -51,7 +49,13 @@ def smogn_boost(data, test_data, TotalIterations, pert, replace, k, y, error_thr
     iteration = 1
     
     # set an initial result
-    result = 0
+    result = []
+    
+    # initialize array of beta values
+    beta = []
+    
+    # initialize array of decision tree predictions based on x_test
+    dt_test_predictions = []
     
     # Dt(i) set distribution as 1/m weights, which is length of data -1, as one of them is the target variable y 
     weights = 1/(len(data))
@@ -72,9 +76,9 @@ def smogn_boost(data, test_data, TotalIterations, pert, replace, k, y, error_thr
         dt_over_sampled = smogn(data=data, y_train = y_train, k = 5, pert = pert, replace=replace, rel_thres = rel_thres, rel_method = "manual", rel_ctrl_pts_rg = rel_ctrl_pts_rg)
 
         # splitting oversampled data for subsequent training data use below
-        df = dt_over_sampled, header = 0
-        x_oversampled = df.drop('y_train', axis = 1)
-        y_oversampled = df['y_train']
+        df_oversampled = dt_over_sampled, header = 0
+        x_oversampled = df_oversampled.drop(y_train, axis = 1)
+        y_oversampled = df_oversampled[y_train]
 
         # calls the decision tree and use it to achieve a new model, predict regression value for y (target response variable), and return the predicted values
         dt_model = tree.DecisionTreeRegressor()
@@ -82,22 +86,26 @@ def smogn_boost(data, test_data, TotalIterations, pert, replace, k, y, error_thr
         # train decision tree classifier
         dt_model = dt_model.fit(x_oversampled, y_oversampled)
         
-        # predict the response for 
+        # predict the features in user provided data
         dt_data_predictions = dt_model.predict(X_data)
+        
+        # predict the features in user provided test data
+        dt_test_predictions.append(dt_model.predict(X_test))
 
-        # initialize error rate
+        # initialize model error rate & epsilon t value
         model_error = 0
+        epsilon_t = 0
 
-        # calculate the error rate of the new model achieved earlier, as the delta between original dataset and predicted oversampled dataset
+        # calculate the model error rate of the new model achieved earlier, as the delta between original dataset and predicted oversampled dataset
         # for each y in the dataset, calculate whether it is greater/lower than threshold and update accordingly
-        error = abs((data[y] - dt_data_predictions[y])/data[y])
+        model_error = abs((data[Y_data] - dt_data_predictions[i])/data[Y_data])
         
         for i in range(1, len[dt_data_predictions], 1):
             if model_error[i] > error_threshold:
                 epsilon_t = epsilon_t + dt_distribution[i]
                                       
         # beta is the update parameter of weights based on the model error rate calculated
-        beta = pow(epsilon_t, 2)
+        beta.append(pow(epsilon_t, 2))
 
         # update the distribution weights
         for i in dt_distribution:
@@ -111,38 +119,11 @@ def smogn_boost(data, test_data, TotalIterations, pert, replace, k, y, error_thr
 
         # iteration count
         iteration += 1
-        
-        # calculate beta log
-        numer = (math.log(1/beta))*(dt_data_predictions[i]) 
-        denom = (math.log(1/beta))
-       
-        # calculate & return result
-        result += (numer/denom)
-    
+               
+    # calculate beta log
+    for b in beta:
+        for i in dt_test_predictions:
+            numer = (math.log(1/beta[b]))*(dt_test_predictions[i]) 
+            denom = (math.log(1/beta[b]))
+        result.append(numer/denom)
     return result
-        
-    # split oversampled data into a training and test set
-    # x_train, X_test, y_train, Y_test = train_test_split(x_oversampled, X_test, y_oversampled, Y_test, test_size=0.3, random_state=0) # 70% training and 30% test
-        
-    # split testing data set into features and target
-    # store data dimensions
-    #n = len(test_data)
-    #d = len(test_data.columns)
-    # determine column position for response variable Y
-    #Y_col = test_data.columns.get_loc(Y)
-    
-    # move response variable Y to last column
-    #if Y_col < d - 1:
-     #   cols = list(range(d))
-      #  cols[Y_col], cols[d - 1] = cols[d - 1], cols[Y_col]
-       # data = data[test_data.columns[cols]]
-    
-    # store original feature headers and
-    # encode feature headers to index position
-    #feat_names = list(test_data.columns)
-    #data.columns = range(d)
-    
-    # sort response variable Y by ascending order
-    #Y = pd.DataFrame(test_data[d - 1])
-    #Y_sort = Y.sort_values(by = d - 1)
-    #Y_sort = Y_sort[d - 1]

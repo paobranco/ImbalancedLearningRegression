2e845c108b70cec205648d2f64992d398fc55a65
Steven Shyaka <sshya024@uottawa.ca>
2023-04-20 15:56:24 -0400

    feat: Remove unnecessary previous tests.

--- ImbalancedLearningRegression/under_sampling/tests/test_enn.py
+++ ImbalancedLearningRegression/under_sampling/tests/test_enn.py
@@ -6,14 +6,6 @@ from sklearn.neighbors import KNeighborsClassifier
 ## Internal Dependencies
 from ImbalancedLearningRegression.under_sampling.enn import ENN, RepeatedENN
 
-def test_repeated_enn_fit_resample():
-    neighbour_classifier = KNeighborsClassifier(n_neighbors = 1, n_jobs = 1)
-    repeated_enn = RepeatedENN(neighbour_classifier = neighbour_classifier)
-    data = read_csv("https://raw.githubusercontent.com/paobranco/ImbalancedLearningRegression/master/data/College.csv")
-    response_variable = "Grad.Rate"
-    
-    new_data = repeated_enn.fit_resample(data = data, response_variable = response_variable)
-
 def test_enn_fit_resample():
     neighbour_classifier = KNeighborsClassifier(n_neighbors = 1, n_jobs = 1)
     enn = ENN(neighbour_classifier = neighbour_classifier)
@@ -21,3 +13,11 @@ def test_enn_fit_resample():
     response_variable = "Grad.Rate"
     
     new_data = enn.fit_resample(data = data, response_variable = response_variable)
+
+def test_repeated_enn_fit_resample():
+    neighbour_classifier = KNeighborsClassifier(n_neighbors = 1, n_jobs = 1)
+    repeated_enn = RepeatedENN(neighbour_classifier = neighbour_classifier)
+    data = read_csv("https://raw.githubusercontent.com/paobranco/ImbalancedLearningRegression/master/data/College.csv")
+    response_variable = "Grad.Rate"
+    
+    new_data = repeated_enn.fit_resample(data = data, response_variable = response_variable)

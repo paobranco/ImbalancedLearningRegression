9e3da714f3f50249b5461b6b09ec84a9f9d75b6c
Steven Shyaka <sshya024@uottawa.ca>
2023-04-20 17:02:24 -0400

    feat: Modify Numerator, Denominator and Resulting Vector.

--- ImbalancedLearningRegression/smogn_boost.py
+++ ImbalancedLearningRegression/smogn_boost.py
@@ -1,18 +1,13 @@
 # load dependencies - third party
 import numpy as np
 import pandas as pd
-import sklearn
 import math
 from ImbalancedLearningRegression.smogn import smogn
 from sklearn import tree
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
 
 # load dependencies - internal
-from ImbalancedLearningRegression.phi import phi
 from ImbalancedLearningRegression.phi_ctrl_pts import phi_ctrl_pts
-from ImbalancedLearningRegression.over_sampling_gn import over_sampling_gn
-from ImbalancedLearningRegression.gn import gn
 
 ## synthetic minority over-sampling technique for regression with gaussian noise with boost (based on SMOTEBoost using Adaboost)
 
@@ -22,7 +17,7 @@ def smogn_boost(
     data,                       ## training dataset (pandas dataframe)
     test_data,                  ## test dataset (pandas dataframe)
     y,                          ## response variable y by name (string)
-    TotalIterations,            ## total number of iterations (pos int)
+    totalIterations,            ## total number of iterations (pos int)
     error_threshold = 0.2,      ## error threshold (pos real)
     pert = 0.02,                ## perturbation / noise percentage (pos real)
     replace = False,            ## sampling replacement (bool)
@@ -33,7 +28,7 @@ def smogn_boost(
 
     ## phi relevance function arguments / inputs
     rel_thres = 0.5,                  ## relevance threshold considered rare (pos real)
-
+    rel_ctrl_pts_rg = None
     ):
 
     """
@@ -42,7 +37,7 @@ def smogn_boost(
     """
     
     ## storing the original training data
-    original_data = data
+    original_data = data.copy()
         
     # read the test data and split features (X) and target value (Y), reference: https://subscription.packtpub.com/book/data/9781838552862/1/ch01lvl1sec10/train-and-test-data
     X_test = test_data.drop(y, axis = 1)
@@ -52,15 +47,12 @@ def smogn_boost(
     X_data = data.drop(y, axis = 1)
     Y_data = data[y]
 
-    # set an initial iteration
-    iteration = 1
-
     ## may initialize as a list instead after more testing, initializing as a numpy array causes issues with NaN for calculations
     # set an array of results, beta values, and decision tree predictions based on x_test
     # beta = np.empty(TotalIterations)
     
     result = []
-    beta = []
+    betas = []
     dt_test_predictions = []
     # dt_test_predictions = np.ones(TotalIterations)
     
@@ -106,21 +98,11 @@ def smogn_boost(
     yDF_sort = yDF.sort_values(by = d - 1)
     yDF_sort = yDF_sort[d - 1]
 
-    # calling phi control
-    pc = phi_ctrl_pts(yDF_sort)
-
-    print("phi control: ", pc)
-    
-    # calling only the control points (third value) from the output
-    rel_ctrl_pts_rg = pc["ctrl_pts"]
-    
-    print("Phi Control Points: ", rel_ctrl_pts_rg)
-
     # loop while iteration is less than user provided iterations
-    while iteration <= TotalIterations:
+    for _ in range(totalIterations):
 
         # use initial training data set provided by user to obtain oversampled dataset using SMOGN, calculating it for the bumps
-        dt_over_sampled = smogn(data=original_data, y=y, k=k)
+        dt_over_sampled = smogn(data=original_data, y = y, k = k, rel_ctrl_pts_rg = rel_ctrl_pts_rg)
 
         # splitting oversampled data for subsequent training data use below
         df_oversampled = pd.DataFrame(dt_over_sampled)
@@ -153,7 +135,7 @@ def smogn_boost(
         for i in range(len(dt_data_predictions)):
             model_error[i] = abs((Y_data[i] - dt_data_predictions[i])/Y_data[i])
         
-        print("Model Error: ", model_error[i])
+            print("Model Error: ", model_error[i])
 
         for i in range(len(dt_data_predictions)):
             if model_error[i] > error_threshold:
@@ -166,10 +148,10 @@ def smogn_boost(
         # set curr_beta as the beta value for each iteration, it is the update parameter of weights based on the model error rate calculated
         # beta = np.append(beta, curr_beta)
         curr_beta = round(pow(epsilon_t, 2), 10)
-        beta.append(curr_beta)
+        betas.append(curr_beta)
         
         print("Current Beta: ", epsilon_t)     
-        print("beta: ", beta)
+        print("betas: ", betas)
         
         #print(dt_distribution)
         # update the distribution weights
@@ -181,55 +163,26 @@ def smogn_boost(
         dt_normalized = preprocessing.normalize(dt_distribution.reshape(1,-1), norm="max")
         
         print("Dt Normalized: ", dt_normalized)
-
-        # iteration count
-        iteration += 1
-
-    # calculate result
-    #numer = 0
-    denom = 0  
     
     # beta 1 * all prediction values in array of first predictions, beta 2 *..... will still have as many arrays as iterations
     # add the arrays together, matching index of each array
     # divide this array by the constant calculated in denom
     # return 1 array
-    
-    numerator = []
-    denominator = 0
-    
-    #print("len dt test pred: ", len(dt_test_predictions)) 
-    
-    ## calculating numerator, looping through the array of arrays, outputs an array of arrays
-    for i in range(len(dt_test_predictions)):
-        calculation = []
-        for j in range(len(dt_test_predictions[i])):
-            calculation.append((math.log((1/beta[i]) * dt_test_predictions[i][j])))
-            result.append(calculation)
-    
-    print("calculation: ", calculation)
-    
-    ## sum all the arrays calculated in numerator
-
-    for i in range(len(calculation)):
-        for j in range(len(calculation[i])):
-            numerator.append((calculation[i]) + calculation[j][i])
-            result.append(numerator)
-    
-    print("numerator: ", numerator)
-    
-    
-    #for i in range((calculation[0])):
-     #   array_sum = 0.0
-      #  for array in calculation:
-       #     array_sum += array[j][i]
-        #numerator.append(array_sum)
-    
-    ## calculate denominator
-    for i in range(len(beta)):
-        denominator += (math.log((1/beta[i])))
         
-    ## calculate result      
-    for i in range(len(numerator)):
-        result.append(numerator[i]/denominator)  
-    print(result)
+    ## calculating numerator, looping through the array of arrays, outputs an array of arrays
+    calculations = []
+    for idx, predictions in enumerate(dt_test_predictions):
+        calculations.append([math.log(1/betas[idx]) * prediction for prediction in predictions])
+
+    # Calculate Numerator
+    numerator = [0] * len(calculations[0])
+    for vector in calculations:
+        for idx, value in enumerate(vector):
+            numerator[idx] += value
+
+    # Calculate Denominator
+    denominator = sum([math.log(1/beta) for beta in betas])
+
+    result = [value/denominator for value in numerator]
+
     return result
